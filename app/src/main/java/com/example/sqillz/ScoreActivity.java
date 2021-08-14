b7225651413cefc6438554048a3e1c6d59b23925
edenGlu <45977312+edenGlu@users.noreply.github.com>
2021-08-14 18:59:36 +0300

    Add "score" activity UI

---
+++ app/src/main/java/com/example/sqillz/ScoreActivity.java
@@ -0,0 +1,14 @@
+package com.example.sqillz;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.score_activity);
+    }
+}
