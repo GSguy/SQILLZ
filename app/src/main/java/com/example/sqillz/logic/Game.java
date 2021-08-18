3a2e0d30161babaa08ccfac4a9ddebc481d295e5
Shachar Nissan <Shachar208@gmail.com>
2021-08-18 21:45:08 +0300

    auto generate questions

--- app/src/main/java/com/example/sqillz/logic/Game.java
+++ app/src/main/java/com/example/sqillz/logic/Game.java
@@ -1,16 +1,27 @@
 package com.example.sqillz.logic;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Random;
 
 public class Game {
+    private static int EASY_NUM_OF_QUESTIONS = 60;
+    private static int EASY_MAX_VALUE = 15;
+
+    private static int MID_NUM_OF_QUESTIONS = 40;
+    private static int MID_MAX_PLUS_VALUE = 30;
+    private static int MID_MAX_MUL_VALUE = 7;
+    private static int MID_MAX_MUL_TIMES = 7;
+
+    private static int HARD_NUM_OF_QUESTIONS = 25;
+    private static int HARD_MAX_PLUS_VALUE = 75;
+    private static int HARD_MAX_MUL_VALUE = 10;
+    private static int HARD_MAX_MUL_TIMES = 10;
+
     private String user;
     private DifficultyEnum difficulty;
     private int score;
     private ArrayList<Question> easy_questions;
-    private ArrayList<Question> med_questions;
+    private ArrayList<Question> mid_questions;
     private ArrayList<Question> hard_questions;
     private ArrayList<Integer> used_questions;
     private ArrayList<Answer> answers;
@@ -25,7 +36,7 @@ public class Game {
         this.difficulty = difficulty;
         this.score = 0;
         this.easy_questions = new ArrayList<>();
-        this.med_questions = new ArrayList<>();
+        this.mid_questions = new ArrayList<>();
         this.hard_questions = new ArrayList<>();
         this.used_questions = new ArrayList<>();
         this.answers = new ArrayList<>();
@@ -34,49 +45,85 @@ public class Game {
     }
 
     private void setQNA(){
-        setQNA_Easy();
-        setQNA_Medium();
-        setQNA_Hard();
+        switch (difficulty){
+            case EASY:
+                setQNA_Easy();
+                break;
+            case MEDIUM:
+                setQNA_Medium();
+                break;
+            case HARD:
+                setQNA_Hard();
+                break;
+        }
     }
 
     private void setQNA_Easy(){
         //Answer answer = new Answer();
-        this.easy_questions.add(new Question("1+1=?", DifficultyEnum.EASY, 2, new int[]{1,7,3}));
-        this.easy_questions.add(new Question("5-1=?", DifficultyEnum.EASY, 4, new int[]{1,6,5}));
-        this.easy_questions.add(new Question("3+2=?", DifficultyEnum.EASY, 5, new int[]{1,2,6}));
-        this.easy_questions.add(new Question("8-8=?", DifficultyEnum.EASY, 0, new int[]{16,10,1}));
-        this.easy_questions.add(new Question("7+2=?", DifficultyEnum.EASY, 9, new int[]{5,10,6}));
-        this.easy_questions.add(new Question("10+0=?", DifficultyEnum.EASY, 10, new int[]{20,0,18}));
-        this.easy_questions.add(new Question("11-1=?", DifficultyEnum.EASY, 10, new int[]{20,12,18}));
-        this.easy_questions.add(new Question("14+7=?", DifficultyEnum.EASY, 21, new int[]{20,10,11}));
-        this.easy_questions.add(new Question("23-5=?", DifficultyEnum.EASY, 18, new int[]{28,16,20}));
-        this.easy_questions.add(new Question("11-9=?", DifficultyEnum.EASY, 2, new int[]{11,9,20}));
+//        this.easy_questions.add(new Question("1+1=?", DifficultyEnum.EASY, 2, new int[]{1,7,3}));
+//        this.easy_questions.add(new Question("5-1=?", DifficultyEnum.EASY, 4, new int[]{1,6,5}));
+//        this.easy_questions.add(new Question("3+2=?", DifficultyEnum.EASY, 5, new int[]{1,2,6}));
+//        this.easy_questions.add(new Question("8-8=?", DifficultyEnum.EASY, 0, new int[]{16,10,1}));
+//        this.easy_questions.add(new Question("7+2=?", DifficultyEnum.EASY, 9, new int[]{5,10,6}));
+//        this.easy_questions.add(new Question("10+0=?", DifficultyEnum.EASY, 10, new int[]{20,0,18}));
+//        this.easy_questions.add(new Question("11-1=?", DifficultyEnum.EASY, 10, new int[]{20,12,18}));
+//        this.easy_questions.add(new Question("14+7=?", DifficultyEnum.EASY, 21, new int[]{20,10,11}));
+//        this.easy_questions.add(new Question("23-5=?", DifficultyEnum.EASY, 18, new int[]{28,16,20}));
+//        this.easy_questions.add(new Question("11-9=?", DifficultyEnum.EASY, 2, new int[]{11,9,20}));
+
+        for (int i = 0; i < EASY_NUM_OF_QUESTIONS; i++) {
+            Question q = Question.generatePlusMinusQuestion(EASY_MAX_VALUE);
+            q.setDifficulty(DifficultyEnum.EASY);
+            this.easy_questions.add(q);
+        }
     }
 
     private void setQNA_Medium(){
-        this.med_questions.add(new Question("1*1=?", DifficultyEnum.MEDIUM, 1, new int[]{-1,-2,2}));
-        this.med_questions.add(new Question("5*7=?", DifficultyEnum.MEDIUM, 35, new int[]{25,40,13}));
-        this.med_questions.add(new Question("9*8=?", DifficultyEnum.MEDIUM, 72, new int[]{0,18,89}));
-        this.med_questions.add(new Question("8:8=?", DifficultyEnum.MEDIUM, 1, new int[]{-1,8,64}));
-        this.med_questions.add(new Question("7*2=?", DifficultyEnum.MEDIUM, 14, new int[]{10,9,3}));
-        this.med_questions.add(new Question("10*0=?", DifficultyEnum.MEDIUM, 0, new int[]{10,100,1}));
-        this.med_questions.add(new Question("21:3=?", DifficultyEnum.MEDIUM, 7, new int[]{21,18,0}));
-        this.med_questions.add(new Question("8*3=?", DifficultyEnum.MEDIUM, 24, new int[]{32,11,18}));
-        this.med_questions.add(new Question("9*2=?", DifficultyEnum.MEDIUM, 18, new int[]{24,9,15}));
-        this.med_questions.add(new Question("15*0=?", DifficultyEnum.MEDIUM, 0, new int[]{10,15,1}));
+//        this.med_questions.add(new Question("1*1=?", DifficultyEnum.MEDIUM, 1, new int[]{-1,-2,2}));
+//        this.med_questions.add(new Question("5*7=?", DifficultyEnum.MEDIUM, 35, new int[]{25,40,13}));
+//        this.med_questions.add(new Question("9*8=?", DifficultyEnum.MEDIUM, 72, new int[]{0,18,89}));
+//        this.med_questions.add(new Question("8:8=?", DifficultyEnum.MEDIUM, 1, new int[]{-1,8,64}));
+//        this.med_questions.add(new Question("7*2=?", DifficultyEnum.MEDIUM, 14, new int[]{10,9,3}));
+//        this.med_questions.add(new Question("10*0=?", DifficultyEnum.MEDIUM, 0, new int[]{10,100,1}));
+//        this.med_questions.add(new Question("21:3=?", DifficultyEnum.MEDIUM, 7, new int[]{21,18,0}));
+//        this.med_questions.add(new Question("8*3=?", DifficultyEnum.MEDIUM, 24, new int[]{32,11,18}));
+//        this.med_questions.add(new Question("9*2=?", DifficultyEnum.MEDIUM, 18, new int[]{24,9,15}));
+//        this.med_questions.add(new Question("15*0=?", DifficultyEnum.MEDIUM, 0, new int[]{10,15,1}));
+
+        for (int i = 0; i < MID_NUM_OF_QUESTIONS; i++) {
+            Question q;
+            boolean isPlus = new Random().nextBoolean();
+            if (isPlus)
+                q = Question.generatePlusMinusQuestion(MID_MAX_PLUS_VALUE);
+            else
+                q = Question.generateMultiDivisionQuestion(MID_MAX_MUL_VALUE,MID_MAX_MUL_TIMES);
+            q.setDifficulty(DifficultyEnum.MEDIUM);
+            this.mid_questions.add(q);
+        }
     }
 
     private void setQNA_Hard(){
-        this.hard_questions.add(new Question("2x+1=5", DifficultyEnum.HARD, 2, new int[]{1,-2,3}));
-        this.hard_questions.add(new Question("(5-1)*x=12", DifficultyEnum.HARD, 3, new int[]{0,5,4}));
-        this.hard_questions.add(new Question("3x-2=-20", DifficultyEnum.HARD, -6, new int[]{10,6,8}));
-        this.hard_questions.add(new Question("2x-8=0", DifficultyEnum.HARD, 4, new int[]{0,-2,8}));
-        this.hard_questions.add(new Question("(10x+2)*2=4", DifficultyEnum.HARD, 0, new int[]{4,-2,10}));
-        this.hard_questions.add(new Question("3x=21", DifficultyEnum.HARD, 7, new int[]{0,-2,9}));
-        this.hard_questions.add(new Question("11-x=12", DifficultyEnum.HARD, -1, new int[]{1,-2,0}));
-        this.hard_questions.add(new Question("7x+7=7", DifficultyEnum.HARD, 0, new int[]{-7,-2,1}));
-        this.hard_questions.add(new Question("4x-5=15", DifficultyEnum.HARD, 5, new int[]{20,4,-2}));
-        this.hard_questions.add(new Question("11x-x=10", DifficultyEnum.HARD, 1, new int[]{10,11,-1}));
+//        this.hard_questions.add(new Question("2x+1=5", DifficultyEnum.HARD, 2, new int[]{1,-2,3}));
+//        this.hard_questions.add(new Question("(5-1)*x=12", DifficultyEnum.HARD, 3, new int[]{0,5,4}));
+//        this.hard_questions.add(new Question("3x-2=-20", DifficultyEnum.HARD, -6, new int[]{10,6,8}));
+//        this.hard_questions.add(new Question("2x-8=0", DifficultyEnum.HARD, 4, new int[]{0,-2,8}));
+//        this.hard_questions.add(new Question("(10x+2)*2=4", DifficultyEnum.HARD, 0, new int[]{4,-2,10}));
+//        this.hard_questions.add(new Question("3x=21", DifficultyEnum.HARD, 7, new int[]{0,-2,9}));
+//        this.hard_questions.add(new Question("11-x=12", DifficultyEnum.HARD, -1, new int[]{1,-2,0}));
+//        this.hard_questions.add(new Question("7x+7=7", DifficultyEnum.HARD, 0, new int[]{-7,-2,1}));
+//        this.hard_questions.add(new Question("4x-5=15", DifficultyEnum.HARD, 5, new int[]{20,4,-2}));
+//        this.hard_questions.add(new Question("11x-x=10", DifficultyEnum.HARD, 1, new int[]{10,11,-1}));
+
+        for (int i = 0; i < HARD_NUM_OF_QUESTIONS; i++) {
+            Question q;
+            boolean isPlus = new Random().nextBoolean();
+            if (isPlus)
+                q = Question.generatePlusMinusQuestion(HARD_MAX_PLUS_VALUE);
+            else
+                q = Question.generateMultiDivisionQuestion(HARD_MAX_MUL_VALUE,HARD_MAX_MUL_TIMES);
+            q.setDifficulty(DifficultyEnum.HARD);
+            this.hard_questions.add(q);
+        }
     }
 
     private boolean is_score_in_highscore(){
@@ -102,7 +149,7 @@ public class Game {
             case EASY:
                 return easy_questions;
             case MEDIUM:
-                return med_questions;
+                return mid_questions;
             case HARD:
                 return hard_questions;
             default:
--- app/src/main/java/com/example/sqillz/logic/Question.java
+++ app/src/main/java/com/example/sqillz/logic/Question.java
@@ -1,5 +1,9 @@
 package com.example.sqillz.logic;
 
+import android.util.Log;
+
+import java.util.Random;
+
 public class Question {
     private static int UUID = 0;
     private String question;
@@ -9,7 +13,7 @@ public class Question {
     private DifficultyEnum difficulty;
 
 
-    public Question(String question, DifficultyEnum difficulty, int answer, int[] wrong_answers){
+    public Question(String question, DifficultyEnum difficulty, int answer, int[] wrong_answers) {
         this.questionID = UUID;
         UUID = UUID + 1;
         this.question = question;
@@ -18,7 +22,7 @@ public class Question {
         this.wrong_answers = wrong_answers;
     }
 
-    public int[] getPosAnswers(){
+    public int[] getPosAnswers() {
         int[] pa = new int[4];
         pa[0] = this.answer;
         pa[1] = this.wrong_answers[0];
@@ -30,5 +34,72 @@ public class Question {
 
     public String getQuestion() {
         return this.question;
+    }
+
+    public static Question generatePlusMinusQuestion(int maximumRandomSize) {
+        int num1, num2, realAnswer, fakeAnswer1, fakeAnswer2, fakeAnswer3;
+        boolean isPlus;
+        String question;
+
+        Random rand = new Random();
+        num1 = rand.nextInt(maximumRandomSize);
+        num2 = rand.nextInt(maximumRandomSize);
+        isPlus = rand.nextBoolean();
+
+        if (num1 < num2) {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+
+        if (isPlus) {
+            realAnswer = num1 + num2;
+            question = String.format("%d + %d = ?", num1, num2);
+        } else {
+            realAnswer = num1 - num2;
+            question = String.format("%d - %d = ?", num1, num2);
+        }
+        Log.i("plus", String.format("num1 = %d, num2 = %d", num1, num2));
+        do{fakeAnswer1 = rand.nextInt(10) + realAnswer - 5;}while (fakeAnswer1 == realAnswer);
+        do{fakeAnswer2 = rand.nextInt(10) + realAnswer - 5;}while ((fakeAnswer2 == realAnswer) || (fakeAnswer2 == fakeAnswer1));
+        do{fakeAnswer3 = rand.nextInt(10) + realAnswer - 5;}while ((fakeAnswer3 == realAnswer) || (fakeAnswer3 == fakeAnswer1) || (fakeAnswer3 == fakeAnswer2));
+
+        return new Question(question, null, realAnswer, new int[]{fakeAnswer1, fakeAnswer2, fakeAnswer3});
+    }
+
+    public static Question generateMultiDivisionQuestion(int maximumRandomSize, int maxMultiply) {
+        int num1, num2, realAnswer, fakeAnswer1, fakeAnswer2, fakeAnswer3;
+        boolean isMulti;
+        String question;
+
+        Random rand = new Random();
+        num1 = (rand.nextInt(maximumRandomSize - 1)) + 1;
+        isMulti = rand.nextBoolean();
+
+        if (isMulti) {
+            num2 = (rand.nextInt(maximumRandomSize - 1)) + 1;
+            realAnswer = num1 * num2;
+            question = String.format("%d * %d = ?", num1, num2);
+        } else {
+            int randomTemp = (rand.nextInt(maximumRandomSize - 1) + 1) % maxMultiply;
+            num2 = randomTemp * num1;
+            if (num2 > num1) {
+                int temp = num1;
+                num1 = num2;
+                num2 = temp;
+            }
+            realAnswer = num1 / num2;
+            question = String.format("%d : %d = ?", num1, num2);
+        }
+        Log.i("multi", String.format("num1 = %d, num2 = %d", num1, num2));
+        do{fakeAnswer1 = rand.nextInt(10) + realAnswer - 5;}while (fakeAnswer1 == realAnswer);
+        do{fakeAnswer2 = rand.nextInt(10) + realAnswer - 5;}while ((fakeAnswer2 == realAnswer) || (fakeAnswer2 == fakeAnswer1));
+        do{fakeAnswer3 = rand.nextInt(10) + realAnswer - 5;}while ((fakeAnswer3 == realAnswer) || (fakeAnswer3 == fakeAnswer1) || (fakeAnswer3 == fakeAnswer2));
+
+        return new Question(question, null, realAnswer, new int[]{fakeAnswer1, fakeAnswer2, fakeAnswer3});
+    }
+
+    public void setDifficulty(DifficultyEnum difficulty) {
+        this.difficulty = difficulty;
     }
 }
